d687a4cc2314930e4f99638b7816045e039a748b
faygun <faruk.aygun@hadilive.com>
2025-02-18 16:31:47 +0000

    translator service example.

---
+++ azure-ai-practices/Program.cs
@@ -0,0 +1,17 @@
+﻿using azure_ai_practices.translator;
+
+namespace azure_ai_practices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "I would really like to drive your car around the block a few times!";
+            var translatorResult = TranslatorService.Translate(text).Result;
+
+            Console.WriteLine(translatorResult);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ azure-ai-practices/translator/TranslatorService.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace azure_ai_practices.translator
+{
+    public class TranslatorService
+    {
+        const string endpoint = "https://api.cognitive.microsofttranslator.com";
+        const string route = "/translate?api-version=3.0&from=en&to=fr&to=zu&to=tr";
+        public static async Task<string> Translate(string text)
+        {
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    string apiKey = Environment.GetEnvironmentVariable("ApiKey");
+                    string region = Environment.GetEnvironmentVariable("Region");
+
+                    object[] body = new object[] { new { Text = text } };
+                    var requestBody = JsonConvert.SerializeObject(body);
+
+                    using (var request = new HttpRequestMessage())
+                    {
+                        // Build the request.
+                        request.Method = HttpMethod.Post;
+                        request.RequestUri = new Uri(endpoint + route);
+                        request.Content = new StringContent(requestBody, Encoding.UTF8, "application/json");
+                        request.Headers.Add("Ocp-Apim-Subscription-Key", apiKey);
+                        // location required if you're using a multi-service or regional (not global) resource.
+                        request.Headers.Add("Ocp-Apim-Subscription-Region", region);
+
+                        // Send the request and get response.
+                        HttpResponseMessage response = await client.SendAsync(request).ConfigureAwait(false);
+                        // Read response as a string.
+                        string result = await response.Content.ReadAsStringAsync();
+                        return result;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return string.Empty;
+            }
+        }
+    }
+}
